33aea7a2c7414c2ba8c0c813d59384bc196b8d8f
Ayush Salik <asalik13@gmail.com>
2020-06-19 17:49:27 +0530

    frontend

---
+++ client/build/precache-manifest.7655a9eb3d7c5450bce7ad7abf956189.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5301d04a9404459c9fe82d9ca5aeb68d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6e58597cef9d5dcf862",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d68cd121236b00e3b83e",
+    "url": "/static/js/2.7d4c55ba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7d4c55ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e58597cef9d5dcf862",
+    "url": "/static/js/main.32573d78.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
